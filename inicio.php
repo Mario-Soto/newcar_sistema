98ad7c437bbc7f2bffb2bd34176c038bcb88a72c
bluedragon <so333398@uaeh.edu.mx>
2021-10-27 09:28:35 -0500

    manual

--- inicio.php
+++ inicio.php
@@ -5,16 +5,19 @@ if (isset($_SESSION['usuario'])) :
     include 'res/layout/nav.php';
     include 'res/layout/header.html';
 ?>
-<h2>Hola, <?=$_SESSION['usuario']['nombre']?></h2>
-
+    <h2>Hola, <?= $_SESSION['usuario']['nombre'] ?></h2>
+    <div class="container">
+        <p>Revisa nuestro manual de usuario para conocer el funcionamiento de este sistema</p>
+        <iframe src="https://docs.google.com/viewer?srcid=18msfhdvlBb1uQK0Q9okZdT9Xk02Vi55o&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="100%" height="620px"></iframe>
+    </div>
 <?php
     include 'res/layout/divs.html';
 else :
     include 'res/layout/tira.html'
 ?>
-<div class="container">
-    <h2 class="text-center">Acceso denegado</h2>
-</div>
+    <div class="container">
+        <h2 class="text-center">Acceso denegado</h2>
+    </div>
 <?php
 endif;
 include 'res/layout/scripts.html';
